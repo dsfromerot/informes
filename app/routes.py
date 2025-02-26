9b55d506ab79d463542b7831162cbb296664bd16
dsfromerot <ds.fernandoromerot@gmail.com>
2025-02-26 12:56:02 +0100

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,5 @@
 from flask import jsonify
-from flask_oidc import OpenIDConnect
-from .config import app, oidc
-
+from app.config import app, oidc
 # Datos hardcodeados
 informes = [
     {"nombre_doctor": "Dr. Smith", "vida_o_muerte": "Vida", "descripcion": "Paciente estable"},
